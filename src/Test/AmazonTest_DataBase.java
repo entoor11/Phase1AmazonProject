2d25eb98ad90eb59d429515c0cd79818a6d3c244
eclipse <eclipse@example.com>
2023-04-24 09:23:39 +1000

    second commit..

--- src/Test/AmazonTest_DataBase.java
+++ src/Test/AmazonTest_DataBase.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.List;
 import org.apache.commons.io.FileUtils;
